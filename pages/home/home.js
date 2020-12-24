f30ddb6a5bf93ef90fe4573e6e25ef840e31b7b9
索月 <suoyue@ysstech.com>
2020-12-24 18:00:22 +0800

    add list

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,7 +7,8 @@ Page({
     motto: 'Hello World~~~~',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    fundList: []
   },
   //事件处理函数
   bindViewTap: function() {
@@ -28,10 +29,45 @@ Page({
         method: 'POST',
         success (res) {
           console.log('res.data && res.data.data', res.data && res.data.data)
-          wx.setStorageSync("fundList", res.data && JSON.stringify(res.data.data))
+          
           console.log('fundList', wx.getStorageSync('fundList'))
 
+          const data = res.data && res.data.data;
+          let len = data.fund_id.length;
+          let fundList = [];
+          for(let i=0; i<len; i++) {
+            fundList.push({
+              fund_id: data.fund_id[i],
+              compre_rank: data.compre_rank[i],
+              foundation_date: data.foundation_date[i],
+              fund_name: data.fund_name[i],
+              fund_status: data.fund_status[i],
+              industry1: data.industry1[i],
+              is_resign: data.is_resign[i],
+              max_drawdown: data.max_drawdown[i],
+              max_drawdown_rank: data.max_drawdown_rank[i],
+              nav: data.nav[i],
+              return_a: data.return_a[i],
+              return_a_rank: data.return_a_rank[i],
+              sharpe_a: data.sharpe_a[i],
+              sharpe_a_rank: data.sharpe_a_rank[i],
+              statistic_date: data.statistic_date[i],
+              stdev_a: data.stdev_a[i],
+              stdev_a_rank: data.stdev_a_rank[i],
+              type: data.type[i],
+              type_name: data.type_name[i],
+            })
+          }
+    console.log('fundList---11111', fundList)
+
+          wx.setStorageSync("fundList", fundList)
+          console.log('fundList---2222', wx.getStorageSync('fundList'))
+
         }
+      })
+    } else {
+      this.setData({
+        fundList: wx.getStorageSync('fundList')
       })
     }
     console.log('fundList---', wx.getStorageSync('fundList'))
