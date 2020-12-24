377485a0cb3d41eb6eb020cde01bb326a1f63ab5
索月 <suoyue@ysstech.com>
2020-12-24 16:49:05 +0800

    init

---
+++ pages/home/home.js
@@ -0,0 +1,77 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World~~~~',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if(!wx.getStorageSync('fundList')) {
+      wx.request({
+        url: 'https://mall.api.fofeasy.cn/apimall/fund_screen', //仅为示例，并非真实的接口地址
+        data: {
+        },
+        header: {
+          'content-type': 'application/json', // 默认值
+          'Authorization': '795722262f8b400a9a3dde0b0d007ce1'
+        },
+        method: 'POST',
+        success (res) {
+          console.log('res.data && res.data.data', res.data && res.data.data)
+          wx.setStorageSync("fundList", res.data && JSON.stringify(res.data.data))
+          console.log('fundList', wx.getStorageSync('fundList'))
+
+        }
+      })
+    }
+    console.log('fundList---', wx.getStorageSync('fundList'))
+
+    
+    
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
