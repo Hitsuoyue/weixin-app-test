4119535ca9148e2a753983920ee1239b9e22096d
索月 <suoyue@ysstech.com>
2020-12-30 18:04:31 +0800

    fundlist

--- pages/fundList/index.js
+++ pages/fundList/index.js
@@ -2,6 +2,39 @@
 //获取应用实例
 const app = getApp()
 
+function transFundList(data) {
+  let len = data.fund_id.length;
+  let fundList = [];
+  for(let i=0; i<len; i++) {
+    fundList.push({
+      fund_id: data.fund_id[i],
+      compre_rank: data.compre_rank[i],
+      foundation_date: data.foundation_date[i],
+      fund_name: data.fund_name[i],
+      fund_status: data.fund_status[i],
+      industry1: data.industry1[i],
+      is_resign: data.is_resign[i],
+      max_drawdown: data.max_drawdown[i],
+      max_drawdown_rank: data.max_drawdown_rank[i],
+      nav: data.nav[i],
+      return_a: data.return_a[i],
+      return_a_rank: data.return_a_rank[i],
+      sharpe_a: data.sharpe_a[i],
+      sharpe_a_rank: data.sharpe_a_rank[i],
+      statistic_date: data.statistic_date[i],
+      stdev_a: data.stdev_a[i],
+      stdev_a_rank: data.stdev_a_rank[i],
+      type: data.type[i],
+      type_name: data.type_name[i],
+    })
+  }
+  console.log('fundList---11111', fundList)
+
+  wx.setStorageSync("fundList", fundList)
+  console.log('fundList---2222', wx.getStorageSync('fundList'))  
+  
+}
+
 Page({
   data: {
     motto: 'Hello World~~~~',
@@ -11,9 +44,33 @@ Page({
     fundList: []
   },
   //事件处理函数
-  bindViewTap: function() {
+  onInputChange: function(e) {
+    const { detail: { value } } = e;
+    console.log('change', e, value)
+  },
+  toAddFund: function() {
     wx.navigateTo({
-      url: '../fundList/index'
+      url: '../addFund/index'
+    })
+  },
+  onSearch: function(e) {
+    const { detail: { value } } = e;
+    console.log('change', e, value)
+    wx.request({
+      url: 'https://mall.api.fofeasy.cn/apimall/fund_screen', //仅为示例，并非真实的接口地址
+      data: {
+      },
+      header: {
+        'content-type': 'application/json', // 默认值
+        'Authorization': '795722262f8b400a9a3dde0b0d007ce1'
+      },
+      method: 'POST',
+      success (res) {
+        console.log('res.data && res.data.data', res.data && res.data.data)
+        console.log('fundList', wx.getStorageSync('fundList'))
+        const data = res.data && res.data.data.slice(0, 4);
+        transFundList(data);
+      }
     })
   },
   onLoad: function () {
@@ -29,40 +86,9 @@ Page({
         method: 'POST',
         success (res) {
           console.log('res.data && res.data.data', res.data && res.data.data)
-          
           console.log('fundList', wx.getStorageSync('fundList'))
-
           const data = res.data && res.data.data;
-          let len = data.fund_id.length;
-          let fundList = [];
-          for(let i=0; i<len; i++) {
-            fundList.push({
-              fund_id: data.fund_id[i],
-              compre_rank: data.compre_rank[i],
-              foundation_date: data.foundation_date[i],
-              fund_name: data.fund_name[i],
-              fund_status: data.fund_status[i],
-              industry1: data.industry1[i],
-              is_resign: data.is_resign[i],
-              max_drawdown: data.max_drawdown[i],
-              max_drawdown_rank: data.max_drawdown_rank[i],
-              nav: data.nav[i],
-              return_a: data.return_a[i],
-              return_a_rank: data.return_a_rank[i],
-              sharpe_a: data.sharpe_a[i],
-              sharpe_a_rank: data.sharpe_a_rank[i],
-              statistic_date: data.statistic_date[i],
-              stdev_a: data.stdev_a[i],
-              stdev_a_rank: data.stdev_a_rank[i],
-              type: data.type[i],
-              type_name: data.type_name[i],
-            })
-          }
-    console.log('fundList---11111', fundList)
-
-          wx.setStorageSync("fundList", fundList)
-          console.log('fundList---2222', wx.getStorageSync('fundList'))
-
+          transFundList(data);
         }
       })
     } else {
